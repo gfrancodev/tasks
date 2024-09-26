e70d52ee6a2c6a4d6de45c94251879329894a267
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-26 03:24:23 -0300

    ✨ implement TaskMapper for task entity transformations

---
+++ src/domain/mappers/task-mapper.ts
@@ -0,0 +1,67 @@
+import {
+  binaryUUIDToString,
+  normalizeUuid,
+  stringToBinaryUUID,
+} from 'src/infraestructure/helpers/binary-uuid-helper';
+import { TaskEntity } from '../entities';
+import { CompanyMapper } from './company-mapper';
+import { UserMapper } from './user-mapper';
+import { mapOrNull } from 'src/infraestructure/helpers/map-or-null';
+import { mapOrUndefined } from 'src/infraestructure/helpers/map-or-undefined';
+import { mapAssignedToOrUndefined } from 'src/infraestructure/helpers/map-assign-or-undefined';
+
+export class TaskMapper {
+  static toDomain(raw: any): TaskEntity {
+    return {
+      id: raw?.id,
+      uuid: normalizeUuid(raw?.uuid),
+      title: raw?.title,
+      description: raw?.description,
+      status: raw?.status as Task.Status$Enum,
+      dueDate: raw?.dueDate,
+      companyId: raw?.companyId,
+      company: mapOrNull(raw?.company, CompanyMapper.toDomain),
+      assignedToId: raw?.assignedToId,
+      assignedTo: mapOrNull(raw?.assignedTo, UserMapper.toDomain),
+      createdAt: raw?.createdAt,
+      updatedAt: raw?.updatedAt,
+    };
+  }
+
+  static toPersistence(
+    task: Partial<TaskEntity>,
+  ): General.ReplacePropertyType<Task.Root, 'uuid', Buffer> {
+    return {
+      id: task?.id,
+      uuid: mapOrUndefined(task?.uuid, stringToBinaryUUID),
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      dueDate: task.dueDate,
+      companyId: task.companyId,
+      assignedToId: task.assignedToId,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    };
+  }
+
+  static toResponse(task: Partial<TaskEntity>) {
+    return {
+      uuid: task.uuid,
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      dueDate: task.dueDate,
+      company: mapAssignedToOrUndefined(task.company, (assignedTo) => ({
+        uuid: assignedTo?.uuid,
+        name: assignedTo.name,
+      })),
+      assignedTo: mapAssignedToOrUndefined(task.assignedTo, (assignedTo) => ({
+        uuid: assignedTo.uuid,
+        email: assignedTo.email,
+      })),
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    };
+  }
+}
