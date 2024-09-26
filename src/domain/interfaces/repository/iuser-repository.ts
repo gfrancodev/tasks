a0baf0c6935ef19229df25b0296d350c3f1d2237
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-26 03:21:32 -0300

    ✨ define IUserRepository interface for user repository methods

---
+++ src/domain/interfaces/repository/iuser-repository.ts
@@ -0,0 +1,19 @@
+import { UserEntity } from 'src/domain/entities';
+import { RoleEnum } from '../../enums/role-enum';
+
+export interface IUserRepository {
+  create(companyId: number, user: Partial<UserEntity>): Promise<UserEntity>;
+  findAll(companyId: number): Promise<UserEntity[]>;
+  findById(companyId: number, id: number): Promise<UserEntity | null>;
+  findByUUID(companyId: number, uuid: string): Promise<UserEntity | null>;
+  update(companyId: number, id: number, user: Partial<UserEntity>): Promise<UserEntity>;
+  delete(companyId: number, id: number): Promise<void>;
+  findByEmail(email: string): Promise<UserEntity | null>;
+  updateRole(companyId: number, id: number, role: RoleEnum): Promise<UserEntity>;
+  findByCompanyAndRole(companyId: number, role: RoleEnum): Promise<UserEntity[]>;
+  findByCompanyWithPagination(
+    companyId: number,
+    page: number,
+    pageSize: number,
+  ): Promise<{ users: UserEntity[]; total: number }>;
+}
