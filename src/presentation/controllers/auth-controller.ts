d20dc1da244d67bb430582b7b8149bb07542de2a
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-27 16:56:18 -0300

    📝 uauth documentation

--- src/presentation/controllers/auth-controller.ts
+++ src/presentation/controllers/auth-controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { Controller, Post, Body, Get, HttpCode } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+  ApiBody,
+} from '@nestjs/swagger';
 import { LoginUseCase } from '@/application/usecases/auth/login-usecase';
 import { GetAuthenticatedUserUseCase } from '@/application/usecases/auth/get-authenticated-user-usecase';
 import { LoginDto } from '@/application/dtos/login-dto';
@@ -16,20 +22,45 @@ export class AuthController {
   ) {}
 
   @IsPublic()
+  @HttpCode(200)
   @Post('login')
   @ApiOperation({ summary: 'Autenticar usuário' })
-  @ApiResponse({ status: 200, description: 'Usuário autenticado com sucesso' })
-  @ApiResponse({ status: 401, description: 'Credenciais inválidas' })
+  @ApiBody({
+    type: LoginDto,
+    description: 'Dados para autenticação do usuário',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Usuário autenticado com sucesso',
+    schema: {
+      example: {
+        access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI...",
+        company_id: "123e4567-e89b-12d3-a456-426614174000"
+      }
+    }
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Credenciais inválidas',
+  })
   async login(@Body() loginDto: LoginDto) {
     return await this.loginUseCase.execute(loginDto);
   }
 
   @ApiBearerAuth()
+  @HttpCode(200)
   @Get('me')
   @Roles('SUPER_ADMIN', 'ADMIN', 'USER')
   @ApiOperation({ summary: 'Obter informações do usuário autenticado' })
-  @ApiResponse({ status: 200, description: 'Informações do usuário obtidas com sucesso' })
-  @ApiResponse({ status: 401, description: 'Não autorizado' })
+  @ApiResponse({
+    status: 200,
+    description: 'Informações do usuário obtidas com sucesso',
+   
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Não autorizado',
+  })
   async getAuthenticatedUser(@CurrentUser() currentUser: Auth.CurrentUser) {
     return await this.getAuthenticatedUserUseCase.execute(currentUser);
   }
