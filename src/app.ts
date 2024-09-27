bf349153a5be50a9879f09ef81445e0b6b91f079
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-27 13:36:05 -0300

    ✨ insert filter, guard and interceptors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { Infraestructure } from './infraestructure';
+import { Application } from './application';
+import { Presentation } from './presentation';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from './infraestructure/exceptions/filter/all-exceptions-filter';
+import { AuthGuard } from './infraestructure/guards/auth-guard';
 
 @Module({
-  imports: [],
+  imports: [Infraestructure, Application, Presentation],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class App {}
