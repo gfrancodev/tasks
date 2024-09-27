c1571cebe7f968ca6ca7eceb2644b11bf181ab3b
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-27 13:59:42 -0300

    ✨ inject and exports database providers and jwt settings

--- src/infraestructure/index.ts
+++ src/infraestructure/index.ts
@@ -1,8 +1,67 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserRepository } from './repositories/user-repository';
+import { CompanyRepository } from './repositories/company-repository';
+import { TaskRepository } from './repositories/task-repository';
+import { JwtManagerService } from './services/jwt-manager-service';
+import { JwtService } from '@nestjs/jwt';
+import { MysqlConnection } from './connections/mysql';
+import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Module({
-  providers: [],
-  exports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [
+    MysqlConnection,
+    PrismaClient,
+    JwtService,
+    {
+      provide: 'USER',
+      useFactory: (prisma: PrismaClient) => new UserRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'COMPANY',
+      useFactory: (prisma: PrismaClient) => new CompanyRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'TASK',
+      useFactory: (prisma: PrismaClient) => new TaskRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'JWT',
+      useClass: JwtManagerService,
+    },
+  ],
+  exports: [
+    MysqlConnection,
+    PrismaClient,
+    JwtService,
+    {
+      provide: 'USER',
+      useFactory: (prisma: PrismaClient) => new UserRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'COMPANY',
+      useFactory: (prisma: PrismaClient) => new CompanyRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'TASK',
+      useFactory: (prisma: PrismaClient) => new TaskRepository(prisma),
+      inject: [PrismaClient],
+    },
+    {
+      provide: 'JWT',
+      useClass: JwtManagerService,
+    },
+  ],
 })
 export class Infraestructure {}
