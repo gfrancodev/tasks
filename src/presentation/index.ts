c05c3ecaa35f35f732dcd9d68085fd255382bc1b
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-27 13:54:55 -0300

    🔨 import controllers

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,7 +1,11 @@
 import { Global, Module } from '@nestjs/common';
+import { AuthController } from './controllers/auth-controller';
+import { CompanyController } from './controllers/company-controller';
+import { CompanyUserController } from './controllers/company-user-controller';
+import { CompanyTaskController } from './controllers/company-task-controller';
 
 @Global()
 @Module({
-  controllers: [],
+  controllers: [AuthController, CompanyController, CompanyUserController, CompanyTaskController],
 })
 export class Presentation {}
