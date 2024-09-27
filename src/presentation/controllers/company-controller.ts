cc3b5c35e17e62a3392ec80f0e02ff8a2a708941
Gustavo Franco <guatavojorgee2511@gmail.com>
2024-09-27 14:30:53 -0300

    ✨ add company controller

---
+++ src/presentation/controllers/company-controller.ts
@@ -0,0 +1,71 @@
+import { Controller, Get, Post, Put, Delete, Body, Param, Query, HttpCode } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { Roles } from '@/infraestructure/decorators/roles-decorator';
+import { CreateCompanyUseCase } from '@/application/usecases/company/create-company-usecase';
+import { GetCompanyUseCase } from '@/application/usecases/company/get-company-usecase';
+import { UpdateCompanyUseCase } from '@/application/usecases/company/update-company-usecase';
+import { DeleteCompanyUseCase } from '@/application/usecases/company/delete-company-usecase';
+import { ListCompanyUseCase } from '@/application/usecases/company/list-company-usecase';
+import { CreateCompanyDto } from '@/application/dtos/create-company-dto';
+import { UpdateCompanyDto } from '@/application/dtos/update-company-dto';
+import { canAccessAdminOwnData } from '@/infraestructure/rules/user-rules';
+import { ConditionalAccess } from '@/infraestructure/decorators/conditional-access-decorator';
+
+@ApiBearerAuth()
+@ApiTags('Empresas')
+@Controller('v1/company')
+export class CompanyController {
+  constructor(
+    private readonly createCompanyUseCase: CreateCompanyUseCase,
+    private readonly getCompanyUseCase: GetCompanyUseCase,
+    private readonly updateCompanyUseCase: UpdateCompanyUseCase,
+    private readonly deleteCompanyUseCase: DeleteCompanyUseCase,
+    private readonly listCompanyUseCase: ListCompanyUseCase,
+  ) {}
+
+  @Post()
+  @Roles('SUPER_ADMIN')
+  @ApiOperation({ summary: 'Criar uma nova empresa' })
+  @ApiResponse({ status: 201, description: 'Empresa criada com sucesso' })
+  async createCompany(@Body() createCompanyDto: CreateCompanyDto) {
+    return await this.createCompanyUseCase.execute(createCompanyDto);
+  }
+
+  @Get(':id')
+  @Roles('SUPER_ADMIN', 'ADMIN', 'USER')
+  @ApiOperation({ summary: 'Obter uma empresa específica' })
+  @ApiResponse({ status: 200, description: 'Empresa encontrada' })
+  @ApiResponse({ status: 404, description: 'Empresa não encontrada' })
+  async getCompany(@Param('id') id: string) {
+    return await this.getCompanyUseCase.execute(id);
+  }
+
+  @Put(':id')
+  @Roles('SUPER_ADMIN', 'ADMIN')
+  @ConditionalAccess(canAccessAdminOwnData())
+  @ApiOperation({ summary: 'Atualizar uma empresa' })
+  @ApiResponse({ status: 200, description: 'Empresa atualizada com sucesso' })
+  @ApiResponse({ status: 404, description: 'Empresa não encontrada' })
+  async updateCompany(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
+    return await this.updateCompanyUseCase.execute(id, updateCompanyDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  @Roles('SUPER_ADMIN')
+  @ConditionalAccess(canAccessAdminOwnData())
+  @ApiOperation({ summary: 'Excluir uma empresa' })
+  @ApiResponse({ status: 204, description: 'Empresa excluída com sucesso' })
+  @ApiResponse({ status: 404, description: 'Empresa não encontrada' })
+  async deleteCompany(@Param('id') id: string) {
+    await this.deleteCompanyUseCase.execute(id);
+  }
+
+  @Get()
+  @Roles('ADMIN', 'USER', 'SUPER_ADMIN')
+  @ApiOperation({ summary: 'Listar empresas' })
+  @ApiResponse({ status: 200, description: 'Lista de empresas obtida com sucesso' })
+  async listCompanies(@Query('page') page?: number, @Query('pageSize') pageSize?: number) {
+    return await this.listCompanyUseCase.execute(page, pageSize);
+  }
+}
